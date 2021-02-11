7501fa13e872100a99964ffd9e5a47b683592341
matthewslape <75864370+matthewslape@users.noreply.github.com>
2021-02-11 11:42:11 -0800

    Start of Project -Added in SVG's to HTML -Set ids and classes to certain SVGs -styled those classes in CSS, with hover effect on day and night SVGs -Used gsap in JavaScript to animate trees and smoke.

--- README.md
+++ README.md
@@ -9,3 +9,9 @@
 ## Resources
 
 -Used https://realfavicongenerator.net/ for faveicons.
+
+-Used https://greensock.com/docs/v3/GSAP for greensock plugins and tips.
+
+-Used Figma to insert SVG's to HTML
+
+-Used codepen as a playground for messing with animations and trial and error of potential ideas.
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,134 @@
-console.log('OK');
+console.log("OK");
+
+// Greensock Coming Soon!
+// TIMELINE
+
+/*Growth of tree*/
+gsap.to("#treeTop", { x: 0, y: -50, repeat: -1, duration: 10, yoyo: true });
+
+/*Radomization of Smoke on fire*/
+gsap.fromTo(
+  "#smoke1",
+
+  {
+    x: 0,
+    y: 0,
+    repeat: -1,
+    duration: "random(1, 7)",
+    opacity: "random(0, 1)",
+    yoyo: true
+  },
+  {
+    x: 0,
+    y: 0,
+    repeat: -1,
+    duration: "random(1, 7)",
+    opacity: "random(0, 1)",
+    yoyo: true
+  }
+);
+
+gsap.fromTo(
+  "#smoke2",
+
+  {
+    x: 0,
+    y: 0,
+    repeat: -1,
+    duration: "random(1, 7)",
+    opacity: "random(0, 1)",
+    yoyo: true
+  },
+  {
+    x: 0,
+    y: 0,
+    repeat: -1,
+    duration: "random(1, 7)",
+    opacity: "random(0, 1)",
+    yoyo: true
+  }
+);
+
+gsap.fromTo(
+  "#smoke3",
+
+  {
+    x: 0,
+    y: 0,
+    repeat: -1,
+    duration: "random(1, 7)",
+    opacity: "random(0, 1)",
+    yoyo: true
+  },
+  {
+    x: 0,
+    y: 0,
+    repeat: -1,
+    duration: "random(1, 7)",
+    opacity: "random(0, 1)",
+    yoyo: true
+  }
+);
+
+gsap.fromTo(
+  "#smoke4",
+
+  {
+    x: 0,
+    y: 0,
+    repeat: -1,
+    duration: "random(1, 7)",
+    opacity: "random(0, 1)",
+    yoyo: true
+  },
+  {
+    x: 0,
+    y: 0,
+    repeat: -1,
+    duration: "random(1, 7)",
+    opacity: "random(0, 1)",
+    yoyo: true
+  }
+);
+
+gsap.fromTo(
+  "#smoke5",
+
+  {
+    x: 0,
+    y: 0,
+    repeat: -1,
+    duration: "random(1, 7)",
+    opacity: "random(0, 1)",
+    yoyo: true
+  },
+  {
+    x: 0,
+    y: 0,
+    repeat: -1,
+    duration: "random(1, 7)",
+    opacity: "random(0, 1)",
+    yoyo: true
+  }
+);
+
+gsap.fromTo(
+  "#smoke6",
+
+  {
+    x: 0,
+    y: 0,
+    repeat: -1,
+    duration: "random(1, 7)",
+    opacity: "random(0, 1)",
+    yoyo: true
+  },
+  {
+    x: 0,
+    y: 0,
+    repeat: -1,
+    duration: "random(1, 7)",
+    opacity: "random(0, 1)",
+    yoyo: true
+  }
+);
